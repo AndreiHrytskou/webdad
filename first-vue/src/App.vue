7a5524e04e03c6f890d8a8c58270777a8b23f649
Andrei Hrytskou <andreihrytskou@gmail.com>
2022-03-17 17:35:57 +0300

    TD-2: Done

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -7,33 +7,54 @@
 		</header>
 		<div class="container">
 				<div class="container__item">
-					<span class="checkbox  active"></span>
-					<input type="checkbox" name="item" id="item">
-					<label for="item">Task 1</label>
-					<span class="close"></span>
+					<label>
+						<span class="checkbox active"></span>
+						<input type="checkbox">
+						<span>Task 1</span>
+						<span class="close"></span>
+					</label>
 				</div>
 				<div class="container__item">
-					<span class="checkbox"></span>
-					<input type="checkbox" name="item" id="item2">
-					<label for="item2">Task 2</label>
-					<span class="close"></span>
+					<label>
+						<span class="checkbox"></span>
+						<input type="checkbox">
+						<span>Task 2</span>
+						<span class="close"></span>
+					</label>
 				</div>
 				<div class="container__item">
-					<span class="checkbox"></span>
-					<input type="checkbox" name="item" id="item3">
-					<label for="item3">Task 3</label>
-					<span class="close"></span>
+					<label>
+						<span class="checkbox"></span>
+						<input type="checkbox">
+						<span>Task 3</span>
+						<span class="close"></span>
+					</label>
 				</div>
 		</div>
-		<div class="btn">
-			<button class="btn__add">Add a new task</button>
+		<div class="entery">
+			<input type="text" class="entery__add" placeholder="Add a new task">
 		</div>
 		<footer class="footer">
 			<div class="footer__counter">1/3 left</div>
 			<div class="footer__readiness">
-				<div class="footer__readiness__item activeTask">All</div>
-				<div class="footer__readiness__item">Active</div>
-				<div class="footer__readiness__item">Completed</div>
+				<div class="footer__readiness__item">
+					<label>
+						<input type="radio" class="footer__readiness__item-radio" name="radio">
+						<span class="footer__readiness__item-text">All</span>
+					</label>
+				</div>
+				<div class="footer__readiness__item">
+					<label>
+						<input type="radio" class="footer__readiness__item-radio" name="radio">
+						<span class="footer__readiness__item-text">Active</span>
+					</label>
+				</div>
+				<div class="footer__readiness__item">
+					<label>
+						<input type="radio" class="footer__readiness__item-radio" name="radio">
+						<span class="footer__readiness__item-text">Completed</span>
+					</label>
+				</div>
 			</div>
 		</footer>
 		</div>

1ebd4c44ef36f31f846a677c5c106036dad1778b
Andrei Hrytskou <andreihrytskou@gmail.com>
2022-03-21 22:19:42 +0300

    TD-2: Переделал.

--- first-vue/src/App.vue
+++ first-vue/src/App.vue
@@ -58,195 +58,225 @@
 	</div>
 </template>
 
-<style lang="scss" >
-// @import "@/assest/scss/main.scss";
+<style lang="scss">
+@import "@/assets/scss/main.scss";
 
-// .header {
-//    text-align: center;
-//    max-width: 54rem;
-//    width: 100%;
-//    height: 5rem;
-//    background-color: $headerColor;
+.content {
 
-//    &__title {
-//       font-family: $titleFonts;
-//       color: $fontsColor;
-//    }
-// }
-// .container {
-//    max-width: 54rem;
-//    margin: 3rem;
+   .todo {
+      position: absolute;
+      top: 9%;
+      left: 50%;
+      background-image: url("./assets/icons/todo.svg");
+      background-repeat: no-repeat;
+      max-width: 25rem;
+      width: 100%;
+      height: 32rem;
+      z-index: -1;
+   }
+	.wrapper {
+		max-width: 33.75rem;
+		width: 100%;
+		margin: 6.25rem auto 0 auto;
+		background-color: $bgColorContainer;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.25rem;
 
-//    &__item {
-//       @include fonts($itemFonts, $fontsColor);
-//       border: 1px solid $itemColor;
-//       border-radius: 1rem;
-//       max-width: 48rem;
-//       height: 5rem;
-//       background-color: $itemColor;
-//       display: flex;
-//       align-items: center;
-//       margin-bottom: 2.5rem;
-//       position: relative;
+		.header {
+			text-align: center;
+			max-width: 33.75rem;
+			width: 100%;
+			height: 3.125rem;
+			background-color: $headerColor;
 
-//       input {
-//          display: none;
-//       }
+			&__title {
+				font-family: $titleFonts;
+				color: $fontsColor;
+			}
+		}
+		.container {
+			max-width: 33.75rem;
+			margin: 1.875rem;
 
-//       .checkbox {
-//          position: relative;
-//          display: inline-block;
-//          top: 0;
-//          cursor: pointer;
+			&__item {
+				@include fonts($itemFonts, $fontsColor);
+				border: 1px solid $itemColor;
+				border-radius: 0.625rem;
+				max-width: 30rem;
+				height: 3.125rem;
+				background-color: $itemColor;
+				display: flex;
+				align-items: center;
+				margin-bottom: 1.5625rem;
+				position: relative;
 
-//          &::before {
-//             content: "";
-//             display: inline-block;
-//             width: 2.4rem;
-//             height: 2.4rem;
-//             border: 0.2rem solid $checkBoxColor;
-//             border-radius: 1rem;
-//             margin: 1.3rem 2rem;
-//             vertical-align: middle;
-//          }
+				input {
+					display: none;
+				}
 
-//          &::after {
-//             content: "\2713";
-//             text-align: center;
-//             color: $bgColorContainer;
-//             padding: 1px 0;
-//             background-color: $checkBoxColor;
-//             position: absolute;
-//             top: 1.3rem;
-//             left: 2.1rem;
-//             width: 2.4rem;
-//             height: 2.4rem;
-//             border-radius: 1rem;
-//             border: 0.1rem solid $checkBoxColor;
-//             opacity: 0;
-//             transition: 0.2s;
-//          }
-//       }
+				.checkbox {
+					position: relative;
+					display: inline-block;
+					top: 0;
+					cursor: pointer;
 
-//       input:checked+.checkbox::after {
-//          opacity: 1;
-//       }
+					&::before {
+						content: "";
+						display: inline-block;
+						width: 1.5rem;
+						height: 1.5rem;
+						border: 0.125rem solid $checkBoxColor;
+						border-radius: 0.625rem;
+						margin: 0.8125rem 1.25rem;
+						vertical-align: middle;
+					}
 
-//       .close {
-//          cursor: pointer;
-//          position: absolute;
-//          right: 2rem;
-//          top: 1.3rem;
-//          background-image: url("../icons/close.svg");
-//          width: 2.6rem;
-//          height: 2.6rem;
-//       }
+					&::after {
+						content: "\2713";
+						text-align: center;
+						color: $bgColorContainer;
+						padding: 1px 0;
+						background-color: $checkBoxColor;
+						position: absolute;
+						top: 0.82rem;
+						left: 1.32rem;
+						width: 1.5rem;
+						height: 1.5rem;
+						border-radius: 0.625rem;
+						border: 0.0625rem solid $checkBoxColor;
+						opacity: 0;
+						transition: 0.2s;
+					}
+				}
 
-//       .close:hover {
-//          background-image: url("../icons/closeHover.svg");
+				input:checked+.checkbox::after {
+					opacity: 1;
+				}
 
-//       }
-//    }
+				.close {
+					cursor: pointer;
+					position: absolute;
+					right: 1.25rem;
+					top: 0.8125rem;
+					background-image: url("./assets/icons/close.svg");
+					width: 1.625rem;
+					height: 1.625rem;
+				}
 
-//    &__item:last-child {
-//       margin-bottom: 0;
-//    }
-// }
-// .entery {
-//    display: flex;
-//    flex-direction: column;
-//    margin: 0 auto;
-//    max-width: 54rem;
-//    margin: 3rem;
+				.close:hover {
+					background-image: url("./assets/icons/closeHover.svg");
 
-//    &__add {
-//       @include fonts($itemFonts, $headerColor);
-//       font-size: 2rem;
-//       background-color: $btnColor;
-//       max-width: 48rem;
-//       width: 100%;
-//       height: 5rem;
-//       background-image: url("data:image/svg+xml,%3csvg max-width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='10' ry='10' stroke='%23FFCA93FF' stroke-width='1.5' stroke-dasharray='9' stroke-dashoffset='0' stroke-linecap='butt'/%3e%3c/svg%3e");
-//       border-radius: 1rem;
-//       cursor: pointer;
-//       border: none;
-//       text-align: center;
-//    }
-// }
+				}
+			}
 
-// ::-webkit-input-placeholder {
-//    /* WebKit browsers */
-//    color: $headerColor;
-// }
+			&__item:last-child {
+				margin-bottom: 0;
+			}
+		}
+		.entery {
+			display: flex;
+			flex-direction: column;
+			margin: 0 auto;
+			max-width: 33.75rem;
+			margin: 1.875rem;
 
-// :-moz-placeholder {
-//    /* Mozilla Firefox 4 to 18 */
-//    color: $headerColor;
-// }
+			&__add {
+				@include fonts($itemFonts, $headerColor);
+				font-size: 1.25rem;
+				background-color: $btnColor;
+				max-width: 30rem;
+				width: 100%;
+				height: 3.125rem;
+				background-image: url("data:image/svg+xml,%3csvg max-width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='10' ry='10' stroke='%23FFCA93FF' stroke-width='1.5' stroke-dasharray='9' stroke-dashoffset='0' stroke-linecap='butt'/%3e%3c/svg%3e");
+				border-radius: 0.625rem;
+				cursor: pointer;
+				border: none;
+				text-align: center;
+			}
+		}
+		.footer {
+			padding: 0 1.875rem;
+			max-width: 33.75rem;
+			height: 3.125rem;
+			background-color: $headerColor;
+			@include fonts($titleFonts, $footerBorder);
+			position: relative;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
 
-// ::-moz-placeholder {
-//    /* Mozilla Firefox 19+ */
-//    color: $headerColor;
-// }
+			&__readiness {
+				display: flex;
 
-// :-ms-input-placeholder {
-//    /* Internet Explorer 10+ */
-//    color: $headerColor;
-// }
-// .footer {
-//    padding: 0 3rem;
-//    max-width: 54rem;
-//    height: 5rem;
-//    background-color: $headerColor;
-//    @include fonts($titleFonts, $footerBorder);
-//    position: relative;
-//    display: flex;
-//    align-items: center;
-//    justify-content: space-between;
+				&__item {
+					margin-right: 1.5625rem;
 
+					&-radio {
+						display: none;
+					}
 
-//    &__readiness {
-//       display: flex;
+					&-text {
+						cursor: pointer;
+						position: relative;
+						display: inline-block;
 
-//       &__item {
-//          margin-right: 2.5rem;
+						&::before {
+							content: "";
+							display: block;
 
-//          &-radio {
-//             display: none;
-//          }
+						}
 
-//          &-text {
-//             cursor: pointer;
-//             position: relative;
-//             display: inline-block;
+						&::after {
+							content: "";
+							position: absolute;
+							top: 0;
+							left: -0.4375rem;
+							width: 100%;
+							height: 100%;
+							border: 0.125rem solid $footerBorder;
+							border-radius: 0.625rem;
+							padding: 0 0.3125rem;
+							opacity: 0;
+							transition: 0.2s;
+						}
+					}
 
-//             &::before {
-//                content: "";
-//                display: block;
+					&-radio:checked+.footer__readiness__item-text::after {
+						opacity: 1;
+					}
 
-//             }
+				}
 
-//             &::after {
-//                content: "";
-//                position: absolute;
-//                top: 0;
-//                left: -0.7rem;
-//                width: 100%;
-//                height: 100%;
-//                border: 0.2rem solid $footerBorder;
-//                border-radius: 1rem;
-//                padding: 0 0.5rem;
-//                opacity: 0;
-//                transition: 0.2s;
-//             }
-//          }
+			}
+		}
+	}
+}
+::-webkit-input-placeholder {
+			/* WebKit browsers */
+			color: $headerColor;
+}
 
-//          &-radio:checked+&-text::after {
-//             opacity: 1;
-//          }
+:-moz-placeholder {
+			/* Mozilla Firefox 4 to 18 */
+			color: $headerColor;
+}
 
-//       }
+::-moz-placeholder {
+			/* Mozilla Firefox 19+ */
+			color: $headerColor;
+}
 
-//    }
-// }
-// </style>
+:-ms-input-placeholder {
+			/* Internet Explorer 10+ */
+			color: $headerColor;
+}
+@media (max-width:800px) {
+   .content {
+
+      .todo {
+         display: none;
+      }
+   }
+}
+</style>
